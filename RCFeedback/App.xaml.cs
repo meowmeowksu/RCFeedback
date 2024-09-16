4f94db3968b0ef7b695d122016140611030020de
Shushanik <shushanikpetro@gmail.com>
2024-09-16 21:12:26 +0300

    project

---
+++ RCFeedback/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using RCFeedback.Data;
+using System.IO;
+
+namespace RCFeedback
+{
+    public partial class App : Application
+    {
+        static FeedbackDatabase feedbackDatabase;
+        public static FeedbackDatabase FeedbackDatabase
+        {
+            get
+            {
+                if (feedbackDatabase == null)
+                {
+                    feedbackDatabase = new FeedbackDatabase(
+                        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "FeedbackDatabase.db3"));
+                }
+                return feedbackDatabase;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MenuPage();
+        }
+
+    }
+
+
+}
---
+++ RCFeedback/MenuPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Montserrat")] 
+[assembly: ExportFont("Cinzel-ExtraBold.ttf", Alias = "Cinzel-ExtraBold")] 
+
+namespace RCFeedback
+{
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent(); 
+        }
+
+        private async void StartButton(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new PersonalInfoPage())); 
+        }
+    }
+}
